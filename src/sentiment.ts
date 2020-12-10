d58db0587dfd8a0cd4c0961e711208821371fc81
Kent English <kenglish@receptiviti.com>
2020-12-10 13:56:19 -0500

    Revising sentiment width TODO wording

--- src/sentiment.ts
+++ src/sentiment.ts
@@ -1,6 +1,6 @@
 // TODO
-// Calculate `width` from `props.sallee.sentiment`. SALLEE sentiment can
-// range from -1 to 1, so `width` should range from 0 to `2 * halfWidth`.
+// Calculate a width value from `sentiment`. SALLEE sentiment can range from -1
+// to 1 and the output should range from 0 to `fullWidth`, respectively.
 export const getSentimentWidth = (sentiment: number, fullWidth: number): number =>  {
   return 0;
 };
